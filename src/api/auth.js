ba3d70c996649baaf6caaf86783c6f1d240dbf6d
Guga Zimmermann <gugazimmermann@gmail.com>
2022-09-07 13:51:10 -0300

    amplify auth functions

---
+++ src/api/auth.js
@@ -0,0 +1,32 @@
+import { Auth as AmplifyAuth } from 'aws-amplify';
+
+const SignUp = async (email, password, locale) => {
+	await AmplifyAuth.signUp({ username: email, password, attributes: { email, locale } });
+};
+
+const ResendConfirmationCode = async (email) => {
+	await AmplifyAuth.resendSignUp(email);
+};
+
+const ConfirmSignUp = async (email, code) => {
+	await AmplifyAuth.confirmSignUp(email, code);
+};
+
+const SignIn = async (email, pwd, remember) => {
+	const auth = await AmplifyAuth.signIn(email, pwd);
+	if (auth.challengeName === 'NEW_PASSWORD_REQUIRED') await AmplifyAuth.completeNewPassword(auth, pwd);
+	if (remember) await AmplifyAuth.rememberDevice();
+	else await AmplifyAuth.forgetDevice();
+};
+
+const ForgotPassword = async (email) => {
+	await AmplifyAuth.forgotPassword(email);
+};
+
+const RedefinePassword = async (email, code, pwd) => {
+	await AmplifyAuth.forgotPasswordSubmit(email, code, pwd);
+};
+
+const Auth = { SignUp, ResendConfirmationCode, ConfirmSignUp, SignIn, ForgotPassword, RedefinePassword };
+
+export default Auth;
