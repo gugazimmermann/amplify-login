d034fc4d04f5e37e284d36e7ce025d1856e25c38
Guga Zimmermann <gugazimmermann@gmail.com>
2022-09-07 13:50:18 -0300

    hide eslint warning

--- src/helpers/isValidEmail.js
+++ src/helpers/isValidEmail.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-useless-escape */
 const isValidEmail = (email) => {
   return email.match(
     /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
