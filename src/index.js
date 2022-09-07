191889bfcd79e145f4a20f067bfb232c34246c30
Guga Zimmermann <gugazimmermann@gmail.com>
2022-09-07 13:50:45 -0300

    initialize aws-amplify

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from "react-router-dom";
+import { Amplify } from 'aws-amplify';
+import awsconfig from './aws-exports';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+Amplify.configure(awsconfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
