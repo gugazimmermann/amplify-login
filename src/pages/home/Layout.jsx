c1e398ff9955b8ef2095153e94a1c55af025a9f2
Guga Zimmermann <gugazimmermann@gmail.com>
2022-09-07 16:50:22 -0300

    add nav to layout

--- src/pages/home/Layout.jsx
+++ src/pages/home/Layout.jsx
@@ -1,18 +1,19 @@
 import { useEffect, useState } from "react";
 import { Outlet, useNavigate } from "react-router-dom";
 import Auth from "../../api/auth";
-import { Loading } from "../../components";
-import Nav from './Nav';
+import { Loading, Nav } from "../../components";
 
 export default function Layout() {
   const navigate = useNavigate();
   const [user, setUser] = useState();
   const [loading, setLoading] = useState(false);
 
-  const loadUser = async () => {
-    setLoading(true);
-    setUser(await Auth.GetUser());
-    setLoading(false);
+  const loadUser = async (force) => {
+    if (!user || force === true) {
+      setLoading(true);
+      setUser(await Auth.GetUser());
+      setLoading(false);
+    }
   };
   
   const handleSignOut = async () => {
@@ -29,7 +30,7 @@ export default function Layout() {
       {loading && <Loading />}
       <Nav handleSignOut={handleSignOut} />
       <div className="mx-auto max-w-screen-lg p-4">
-        <Outlet context={{ user }} />
+        <Outlet context={{ user, loadUser , setLoading}} />
       </div>
     </main>
   );
