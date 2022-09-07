84bc717b81b24dbdaf702e4da6cfa95ae9e41445
Guga Zimmermann <gugazimmermann@gmail.com>
2022-09-07 16:48:08 -0300

    nav header

---
+++ src/components/Nav.jsx
@@ -0,0 +1,12 @@
+import { NavProfile } from ".";
+
+const Nav = ({ handleSignOut }) => (
+  <header className="w-full shadow-md z-30 px-2 py-1.5 bg-white text-slate-500">
+    <div className="container mx-auto flex flex-wrap justify-between px-2">
+      <p className="text-2xl">{process.env.REACT_APP_TITLE}</p>
+      <NavProfile handleSignOut={handleSignOut} />
+    </div>
+  </header>
+);
+
+export default Nav;
