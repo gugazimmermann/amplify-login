8dafab8a506534823bd954093e836e5c7609f4ea
Guga Zimmermann <gugazimmermann@gmail.com>
2022-09-06 23:32:56 -0300

    confirm sign up page

--- src/pages/auth/ConfirmSignUp.jsx
+++ src/pages/auth/ConfirmSignUp.jsx
@@ -1,13 +1,43 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useOutletContext } from "react-router-dom";
+import { AuthButton, AuthLink, AuthTitle, Input } from "../../components";
 import ConfirmSignUpImage from "../../images/confirm_signup.svg";
 
 export default function ConfirmSignUp() {
   const { setImg } = useOutletContext();
+  const [email, setEmail] = useState("");
+  const [code, setCode] = useState("");
 
   useEffect(() => {
     setImg(ConfirmSignUpImage);
   }, [setImg]);
 
-  return <form />;
+  const disabled = () => email === "" || code === "";
+
+  return (
+    <form>
+      <AuthTitle text="confirm registration" />
+      <div className="mb-4">
+        <Input
+          type="email"
+          placeholder="Email"
+          value={email}
+          handler={setEmail}
+        />
+      </div>
+      <div className="mb-4">
+        <Input type="text" placeholder="Code" value={code} handler={setCode} />
+      </div>
+      <AuthButton
+        text="Confim"
+        disabled={disabled()}
+        handler={() => {
+          console.log(email, code);
+        }}
+      />
+      <div className="w-full text-center mt-6">
+        <AuthLink text="Back to Sign In" to="/" size="xl" />
+      </div>
+    </form>
+  );
 }
