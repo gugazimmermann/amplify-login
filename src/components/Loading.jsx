551520ca3cbe65889d611fef377df4718dd82c71
Guga Zimmermann <gugazimmermann@gmail.com>
2022-09-06 23:02:35 -0300

    fix colors

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,8 +1,8 @@
 const Loading = () => (
   <div className="fixed inset-0 bg-gray-900 bg-opacity-90 overflow-y-auto h-full w-full z-50">
     <div className="flex flex-col justify-center items-center h-full w-full">
-      <div className="mb-4 text-primary text-2xl font-bold">Loading...</div>
-      <div className="animate-spin rounded-full h-32 w-32 border-b-8 border-primary" />
+      <div className="mb-4 text-indigo-500 text-2xl font-bold">Loading...</div>
+      <div className="animate-spin rounded-full h-32 w-32 border-b-8 border-indigo-500" />
     </div>
   </div>
 );
