b6d93ad104bbbfbd9fe5d67af609269fdaa67025
Guga Zimmermann <gugazimmermann@gmail.com>
2022-09-08 12:13:28 -0300

    change language

--- src/pages/not-found/NotFound.jsx
+++ src/pages/not-found/NotFound.jsx
@@ -1,12 +1,22 @@
-import NotFoundImg from '../../images/not_found.svg';
+import { useContext } from "react";
+import { AppContext } from "../../context";
+import { LANGUAGES } from "../../constants";
+import NotFoundImg from "../../images/not_found.svg";
 
-const NotFound = () => (
-  <div className="container bg-white mx-auto">
-    <main className="flex flex-col h-screen justify-center items-center">
-      <img src={NotFoundImg} alt="not found" className="max-w-xs" />
-      <h1 className='mt-4 text-xl'>Sorry, Page Not Found.</h1>
-    </main>
-  </div>
-);
+const NotFound = () => {
+  const { state } = useContext(AppContext);
+
+  return (
+    <div className="container bg-white mx-auto">
+      <main className="flex flex-col h-screen justify-center items-center">
+        <img src={NotFoundImg} alt="not found" className="max-w-xs" />
+        <h1 className="mt-4 text-xl">
+          {LANGUAGES[state.lang].NotFound.Sorry},{" "}
+          {LANGUAGES[state.lang].NotFound.PageNotFound}.
+        </h1>
+      </main>
+    </div>
+  );
+};
 
 export default NotFound;
