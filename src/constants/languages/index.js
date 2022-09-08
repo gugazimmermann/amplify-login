af31522976172ff0f7c737f007158245eaca19af
Guga Zimmermann <gugazimmermann@gmail.com>
2022-09-08 12:07:47 -0300

    add routes and languages contants

---
+++ src/constants/languages/index.js
@@ -0,0 +1,2 @@
+export { default as pt_br_lang } from "./pt_br";
+export { default as en_us_lang } from "./en_us";
---
+++ src/constants/routes/pt_br.js
@@ -0,0 +1,11 @@
+const pt_br_routes = {
+  SIGN_IN: "/",
+  FORGOT_PASSWORD: "/esqueceu-senha",
+  REDEFINE_PASSWORD: "/redefinir-senha",
+  SIGN_UP: "/registro",
+  CONFIRM_SIGN_UP: "/confirmar-registro",
+  HOME: "/inicial",
+  PROFILE: "/peril",
+};
+
+export default pt_br_routes;
