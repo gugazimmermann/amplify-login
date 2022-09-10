ae0fe198ae6f6dc0ff512106e7c9d985f455b7d0
Guga Zimmermann <gugazimmermann@gmail.com>
2022-09-10 15:02:44 -0300

    verify user name and birthdate

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,14 @@
 import { useContext } from "react";
 import { AppContext} from '../../context';
+import { Alert } from '../../components';
 
 export default function Home() {
   const { state } = useContext(AppContext);
   const { user } = state;
 
-  return <section>{user && <pre>{JSON.stringify(user, undefined, 2)}</pre>}</section>;
+  return (
+    <section>
+      {(!user.name || !user.birthdate) && <Alert type="warning" text="Please, complete the Profile!" />}
+    </section>
+  );
 }
